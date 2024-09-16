715022a8222da8c17f37968eff9af5f7089a0c19
ProeungChiso <proeung.chiso.2821@rupp.edu.kh>
2024-09-16 14:31:25 +0700

    done

---
+++ src/main/java/co/istad/springcloudconfigclient/ClientController.java
@@ -0,0 +1,19 @@
+package co.istad.springcloudconfigclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/credentials")
+public class ClientController {
+
+    @Value("${info}")
+    private String serviceInfo;
+
+    @GetMapping
+    public String getServiceInfo() {
+        return serviceInfo;
+    }
+}
